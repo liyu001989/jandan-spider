f20a74528c12a0087051f12cf05b68a83157428b
Yu Li <liyu001989@gmail.com>
2016-01-10 22:56:57 +0800

    async download image

--- jandan.js
+++ jandan.js
@@ -1,22 +1,23 @@
+#! /usr/bin/env node
 var superagent = require('superagent'),
     cheerio = require('cheerio'),
     fs = require('fs'),
     path = require('path'),
     crypto = require('crypto'),
-    request = require('request');
+    async = require('async');
 
+// 图片名字hash一下，避免重名
 var hash = function(str) {
     return crypto.createHash('md5').update(str).digest('hex');
 }
 
+// 下载图片
 var download = function(uri, filename, callback){
-    console.log(uri);
-    console.log(filename);
-    request.head(uri, function(err, res, body){
-        //console.log('content-type:', res.headers['content-type']);
-        //console.log('content-length:', res.headers['content-length']);
-        request(uri).pipe(fs.createWriteStream(filename));
-    });
+    superagent.get(uri)
+        .end(function(err, res) {
+            fs.writeFile(filename, res.body)
+            callback(null, uri);
+        });
 };
 
 var jandanUrl = 'http://jandan.net/ooxx/page-1500';
@@ -29,6 +30,7 @@ superagent.get(jandanUrl)
 
         var $ = cheerio.load(sres.text);
         var items = [];
+        var urls = [];
 
         $('.commentlist li').each(function(index, element) {
             $element = $(element);
@@ -46,12 +48,26 @@ superagent.get(jandanUrl)
             var imageHash = crypto.createHash('md5').update(imageUrl).digest('hex');
             var filename = 'images/'+imageHash+extension;
 
-            download(imageUrl, filename);
-
             items.push({
-                image: $element.find('img').attr('src'),
+                image: imageUrl,
                 oo: oo,
                 xx: xx
             });
+
+            urls.push(imageUrl);
+        });
+
+        // 并发下载所有的图片，控制再5个并发
+        async.mapLimit(urls, 5, function (url, callback) {
+            //后缀
+            var extension=path.extname(url);
+            //url hash
+            var imageHash = crypto.createHash('md5').update(url).digest('hex');
+            var filename = 'images/'+imageHash+extension;
+            download(url, filename, callback);
+        }, function (err, result) {
+            console.log('final:');
+            console.log(result);
         });
     });
+
