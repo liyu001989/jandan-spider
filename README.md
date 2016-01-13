041c982815eaaa53bf14c41bc66d6796ac5a6e0b
Yu Li <liyu001989@gmail.com>
2016-01-13 23:53:56 +0800

    async whilst

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-#学者爬一下煎蛋
+##学者爬一下煎蛋
 
-[https://github.com/alsotang/node-lessons](https://github.com/alsotang/node-lessons)
 看着这个课程尝试一下, 下载煎蛋的妹子图
-
+- [https://github.com/alsotang/node-lessons](https://github.com/alsotang/node-lessons)
 - [superagent](http://visionmedia.github.io/superagent/)
 - [cheerio](https://github.com/cheeriojs/cheerio)
 - [async](https://github.com/caolan/async)
 
+
+### License
+
+[MIT license](http://opensource.org/licenses/MIT)
--- jandan.js
+++ jandan.js
@@ -4,6 +4,9 @@ var superagent = require('superagent'),
     fs = require('fs'),
     path = require('path'),
     crypto = require('crypto'),
+    mongoose = require('mongoose'),
+    mkdirp = require('mkdirp'),
+    program = require('commander'),
     async = require('async');
 
 // 图片名字hash一下，避免重名
@@ -20,54 +23,77 @@ var download = function(uri, filename, callback){
         });
 };
 
-var jandanUrl = 'http://jandan.net/ooxx/page-1500';
+var parseContent = function(page, url, content)
+{
+    var $ = cheerio.load(content);
+    var items = [];
+    var urls = [];
 
-superagent.get(jandanUrl)
-    .end(function(err, sres) {
-        if (err) {
-            return next(err);
-        }
+    $('.commentlist li').each(function(index, element) {
+        $element = $(element);
+        // 取得图片的id
+        var id = $element.attr('id').split("-")[1];
 
-        var $ = cheerio.load(sres.text);
-        var items = [];
-        var urls = [];
+        // 没id的是广告
+        if (!id) return true;
 
-        $('.commentlist li').each(function(index, element) {
-            $element = $(element);
-            // 取得图片的id
-            var id = $element.attr('id').split("-")[1];
+        var imageUrl = $element.find('img').attr('src');
+        var oo = $element.find('#cos_support-'+id).html();
+        var xx = $element.find('#cos_unsupport-'+id).html();
 
-            // 没id的是广告
-            if (!id) return true;
+        var extension=path.extname(imageUrl);
+        var imageHash = crypto.createHash('md5').update(imageUrl).digest('hex');
+        var filename = 'images/'+imageHash+extension;
 
-            var imageUrl = $element.find('img').attr('src');
-            var oo = $element.find('#cos_support-'+id).html();
-            var xx = $element.find('#cos_unsupport-'+id).html();
-
-            var extension=path.extname(imageUrl);
-            var imageHash = crypto.createHash('md5').update(imageUrl).digest('hex');
-            var filename = 'images/'+imageHash+extension;
-
-            items.push({
-                image: imageUrl,
-                oo: oo,
-                xx: xx
-            });
-
-            urls.push(imageUrl);
+        items.push({
+            image: imageUrl,
+            oo: oo,
+            xx: xx
         });
 
-        // 并发下载所有的图片，控制再5个并发
-        async.mapLimit(urls, 5, function (url, callback) {
-            //后缀
-            var extension=path.extname(url);
-            //url hash
-            var imageHash = crypto.createHash('md5').update(url).digest('hex');
-            var filename = 'images/'+imageHash+extension;
-            download(url, filename, callback);
-        }, function (err, result) {
-            console.log('final:');
-            console.log(result);
-        });
+        urls.push(imageUrl);
     });
 
+    // 并发下载所有的图片，控制再5个并发
+    async.mapLimit(urls, 5, function (url, callback) {
+        //后缀
+        var extension=path.extname(url);
+        // 暂时认为煎蛋的url会唯一对应一个图片，抓过的就不抓了
+        //url hash
+        var imageHash = crypto.createHash('md5').update(url).digest('hex');
+        mkdirp('images/'+page);
+        var filename = 'images/'+ page +'/'+imageHash+extension;
+        download(url, filename, callback);
+    }, function (err, result) {
+        console.log('final:');
+        console.log(result);
+    });
+
+}
+
+// 妹子图地址
+var meiziUrl = 'http://jandan.net/ooxx/page-';
+// 起始页
+var page = 1500;
+// 终止页
+var end = 1504;
+
+async.whilst(
+    function () {return page < end; },
+    function (callback) {
+        page++;
+
+        var targetUrl = meiziUrl + page;
+        superagent.get(targetUrl)
+            .end(function(err, sres) {
+                if (err) {
+                    return next(err);
+                }
+
+                parseContent(page, meiziUrl, sres.text);
+                 callback(null, page);
+            });
+    },
+    function (err, n) {
+    }
+)
